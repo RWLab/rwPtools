181af52a4cd7ed47651b0b8e18ee3f8f3128cfe3
robotajet <ajet@robotwealth.com>
2020-10-30 19:51:10 +0100

    Remove install_requires

--- setup.py
+++ setup.py
@@ -7,11 +7,5 @@ setup(
     author="Robot Wealth",
     author_email="ajet@robotwealth.com",
     packages=['rwptools'],
-    install_requires=[
-          'google-auth>=1.17.2',
-          'google-cloud-storage>=1.32.0',
-          'pandas>=1.0',
-          'pyarrow'
-          ]
 
 )
