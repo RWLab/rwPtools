d64a021ba01e077fc157ccac7b3b97023451d4ba
robotajet <ajet@robotwealth.com>
2020-10-30 19:22:54 +0100

    Dependency change to work with colab

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['rwptools'],
     install_requires=[
           'gcloud>=0.18.3',
-          'google-auth>=1.22.1',
+          'google-auth>=1.17.2',
           'google-cloud-storage>=1.32.0',
           'pandas>=1.0',
           'pyarrow'
